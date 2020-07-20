2c8cd9f32c906e11312d763ceb594903e0a79174
Astradoks <paul.bonilla3000@hotmail.com>
2020-07-20 17:36:54 -0500

    Creation of collectables and snake growing
    All was changed to OOP.
    There were created 2 clases, collectables, and snake.
    It was implemented the functionality of moving and make the snake grow when it touches a collectable.

--- Snake.py
+++ Snake.py
@@ -1,72 +1,119 @@
 import pygame
+import random
 pygame.init()
+
+class Snake(object):
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.height = 10
+        self.width = 10
+        self.velocity = 10
+        self.directionX = 1
+        self.directionY = 0
+
+    def move(self, keys):
+        
+        if keys[pygame.K_RIGHT] and self.directionX != -1:
+            self.directionX = 1
+            self.directionY = 0
+
+        if keys[pygame.K_LEFT] and self.directionX != 1:
+            self.directionX = -1
+            self.directionY = 0
+
+        if keys[pygame.K_UP] and self.directionY != 1:
+            self.directionX = 0
+            self.directionY = -1
+
+        if keys[pygame.K_DOWN] and self.directionY != -1:
+            self.directionX = 0
+            self.directionY = 1
+
+        #Constant move of the snake
+        self.x += self.directionX * self.velocity
+        self.y += self.directionY * self.velocity
+
+class Collectable(object):
+    def __init__(self):
+        self.radius = 5
+        self.x = random.randrange(5, sizeX - self.radius, 10)
+        self.y = random.randrange(5, sizeY - self.radius, 10)
+        self.color = (200, 255, 0)
+
+    def drawCollectable(self, win):
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
+
+#Game draw and update
+def draw(win, completeSnake):
+    win.fill((0, 0, 0))
+    for snakeBlock in completeSnake:
+        pygame.draw.rect(win, (0, 165, 255), (snakeBlock.x, snakeBlock.y, snakeBlock.width, snakeBlock.height))
+    food.drawCollectable(win)
+    pygame.display.update()
 
 #Window declaration
 sizeX = 500
 sizeY = 500
 win = pygame.display.set_mode((sizeX, sizeY))
 pygame.display.set_caption("Sanke Game")
+clock = pygame.time.Clock()
 
 #Snake declaration
-x = 250
-y = 250
-height = 10
-width = 10
+snakeBlock = Snake(250, 250)
+count =  0
+completeSnake = [snakeBlock]
 
-#Some Variables Declaration
-velocity = 10
-directionX = 1
-directionY = 0
+#Collectable declaration
+food = Collectable()
 
 #Loop for the game
-game = True
-while game:
+run = True
+while run:
 
     #A lite delay between every frame
-    pygame.time.delay(50)
+    clock.tick(10)
 
     #Look for all the events that ocurre in the game
     for event in pygame.event.get():
         #Event to close the game
         if event.type == pygame.QUIT:
-            game = False
+            run = False
     
     #Get of the keys that are pressed
     keys = pygame.key.get_pressed()
+    
+    #Refresh all the blocks of the snake
+    if len(completeSnake) > 1:
+        for i in range(len(completeSnake) - 1, -1, -1):
+            if i != 0:
+                completeSnake[i].x = completeSnake[i-1].x
+                completeSnake[i].y = completeSnake[i-1].y
 
-    if keys[pygame.K_RIGHT]:
-        directionX = 1
-        directionY = 0
+    #Move of the snake
+    snakeBlock.move(keys)
 
-    if keys[pygame.K_LEFT]:
-        directionX = -1
-        directionY = 0
-
-    if keys[pygame.K_UP]:
-        directionX = 0
-        directionY = -1
-
-    if keys[pygame.K_DOWN]:
-        directionX = 0
-        directionY = 1
-
-    #Constant move of the snake
-    x += directionX * velocity
-    y += directionY * velocity
+    #Collection of food
+    if food.x < snakeBlock.x + snakeBlock.width and food.x > snakeBlock.x:
+        if food.y < snakeBlock.y + snakeBlock.height and food.y > snakeBlock.y:
+            lastX = completeSnake[count].x
+            lastY = completeSnake[count].y
+            completeSnake.append(Snake(lastX, lastY))
+            food = Collectable()
+            count += 1
 
     #infinite move through the walls
-    if x < 0:
-        x = sizeX - width
-    if x > sizeX - width:
-        x = 0
-    if y < 0:
-        y = sizeY - height
-    if y > sizeY - height:
-        y = 0
-    
+    if snakeBlock.x < 0:
+        snakeBlock.x = sizeX - snakeBlock.width
+    if snakeBlock.x > sizeX - snakeBlock.width:
+        snakeBlock.x = 0
+    if snakeBlock.y < 0:
+        snakeBlock.y = sizeY - snakeBlock.height
+    if snakeBlock.y > sizeY - snakeBlock.height:
+        snakeBlock.y = 0
+
     #Window update
-    win.fill((0, 0, 0))
-    pygame.draw.rect(win, (0, 165, 255), (x, y, width, height))
-    pygame.display.update()
+    draw(win, completeSnake)
 
 pygame.quit()
